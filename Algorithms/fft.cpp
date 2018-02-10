e34d82ad06bd0c5cddc2d246bc56c51671c2b3ba
Pritam <pritampathak96@gmail.com>
2018-02-10 22:53:38 +0530

    Implemented FFT

--- Algorithms/fft.cpp
+++ Algorithms/fft.cpp
@@ -1,109 +1,131 @@
 #include<iostream> //For standard I/O operations
-#include<math.h> //For finding power
-#include<cstring> //For memset
+#include<complex>  //For complex numbers
+#include<vector>   //For storing the vectors
 #define PI 3.14159
 #define e 2.71828
 using namespace std;
+typedef complex < double > base ;
 
-//Calculates point value form from coefficient form of the polynomial
-//in array a of size n and returns in array y of size n
-float * Recursive_fft(float *a,float *y,int n)
-{
-    //Base case
-    if(n==1)
-        return a;
-    float w=1;
-    float w_n=pow(e,2*PI/n);
-    int new_n=n/2;
-    float a_even[new_n],a_odd[new_n];
-    for(int i=0;i<n;i++)
+//Calculates FFT when invert= false
+//Inverse FFT when invert= true
+void fft ( vector < base > & a, bool invert ) {
+    //Getting the size
+	int n=(int) a.size();
+
+    //Base Case:-------------
+	if(n==1)
+        return;
+    //-----------------------
+
+    //a0<-even elements
+    //a1<-odd elements
+	vector<base> a0(n/2), a1(n/2);
+
+    //Filling up a0 and a1:-------------
+	for(int i=0,j=0; i<n; i+=2,++j)
     {
-        if(i%2==0)
-            a_even[i/2]=a[i];
-        else
-            a_odd[i/2]=a[i];
-    }
-    float *y_even=NULL,*y_odd=NULL;
-    y_even=Recursive_fft(a_even,y_even,new_n);
-    y_odd=Recursive_fft(a_odd,y_odd,new_n);
-    float y_arr[n];
-    for(int i=0;i<new_n;i++)
+		a0[j]=a[i];
+		a1[j]=a[i+1];
+	}
+    //----------------------------------
+
+    //Recurive calls(Divide portion):--
+	fft(a0,invert);
+	fft(a1,invert);
+    //---------------------------------
+
+    //Getting the roots of unity:------
+	double ang=2*PI/n*(invert? -1: 1); //Handles the inverse part
+	base w(1), wn(cos(ang),sin(ang));
+    //---------------------------------
+
+    //Conquer portion:--------------------------
+	for(int i=0;i<n/2;++i)
     {
-        y_arr[i]=y_even[i] + w*y_odd[i];
-        y_arr[new_n+i]=y_even[i] - w*y_odd[i];
-        w*=w_n;
-    }
-    y=y_arr;
-    return y;
+		a[i]=a0[i]+w*a1[i];
+		a[i+n/2]=a0[i]-w*a1[i];
+
+        //Handles the inverse part:--
+		if(invert)
+        {
+			a[i]/=2;
+            a[i+n/2]/=2;
+        }
+        //----------------------------
+		w *=wn ;
+	}
+    //--------------------------------------------
 }
-float * Inverse_fft(float *a,float *y,int n)
+
+//Multiplies the polynomials in a and b and returns the result in res
+void multiply(const vector<int> &a, const vector<int> &b, vector<int> &res)
 {
-    //Base case
-    if(n==1)
-        return a;
-    float w=1;
-    float w_n=pow(e,-2*PI/n);
-    int new_n=n/2;
-    float a_even[new_n],a_odd[new_n];
-    for(int i=0;i<n;i++)
-    {
-        if(i%2==0)
-            a_even[i/2]=a[i];
-        else
-            a_odd[i/2]=a[i];
-    }
-    float *y_even=NULL,*y_odd=NULL;
-    y_even=Recursive_fft(a_even,y_even,new_n);
-    y_odd=Recursive_fft(a_odd,y_odd,new_n);
-    float y_arr[n];
-    for(int i=0;i<new_n;i++)
-    {
-        y_arr[i]=(y_even[i] + w*y_odd[i])/n;
-        y_arr[new_n+i]=(y_even[i] - w*y_odd[i])/n;
-        w*=w_n;
-    }
-    y=y_arr;
-    return y;
+    //Storing the polygons in a vector of complex numbers:--------
+	vector<base> fa(a.begin(),a.end()), fb(b.begin(),b.end());
+    //------------------------------------------------------------
+
+    //Getting n in power of 2:-----------
+	size_t n=1;
+	while(n< max(a.size(),b.size()))
+        n <<= 1 ;
+	n <<= 1 ;
+    //-----------------------------------
+
+    //Resizing the vectors
+	fa.resize(n), fb.resize(n);
+
+    //FFT call:-------------
+	fft(fa,false);
+    fft(fb,false);
+    //----------------------
+
+    //Convolution:----------
+	for(size_t i=0;i<n;++i)
+		fa[i]*=fb[i];
+    //----------------------
+
+    //Inverse FFT call:-----
+	fft(fa,true);
+    //----------------------
+
+    //Storing the result in an integer vector:--
+	res.resize(n);
+	for (size_t i=0;i<n;++i)
+		res[i]=int(fa[i].real()+0.5);
+    //------------------------------------------
 }
-float * Convolution(float *a,float *b,float *c,int n)
-{
-    float *a1=NULL,*b1=NULL;
-    a1=Recursive_fft(a,a1,n);
-    b1=Recursive_fft(b,b1,n);
-    float c_arr[n];
-    for(int i=0;i<n;i++)
-        c_arr[i]=a[i]*b[i];
-    c=Inverse_fft(c_arr,c,n);
-    return c;
-}
+
 int main()
 {
-    //Degree of polynomials
-    int n1,n2;
-    cin>>n1>>n2;
-    int n=max(n1,n2);
-    n=pow(2,(int)log2(n)+1);
-    cout<<n<<endl;
-
+    int temp;
     //First polynomial
-    float poly1[n]; //Polynomial array 1
-    memset(poly1,0,sizeof(poly1));
+    int n1;
+    cin>>n1;
+    vector<int> poly1; //Polynomial array 1
     for(int i=0;i<n1;i++)
-        cin>>poly1[i];
+    {
+        cin>>temp;
+        poly1.push_back(temp);
+    }
     //------------------
 
     //Second polynomial
-    float poly2[n]; //Polynomial array 2
-    memset(poly2,0,sizeof(poly2));
+    int n2;
+    cin>>n2;
+    vector<int> poly2; //Polynomial array 2
     for(int i=0;i<n2;i++)
-        cin>>poly2[i];
+    {
+        cin>>temp;
+        poly2.push_back(temp);
+    }
     //------------------
 
     //Product polynomial
-    float *c=NULL;
-    c=Convolution(poly1,poly2,c,n);
+    vector<int> res;
+    multiply(poly1,poly2,res);
+    int n=res.size();
     for(int i=0;i<n;i++)
-        cout<<c[i]<<" ";
+        cout<<res[i]<<" ";
     cout<<endl;
 
     return 0;
